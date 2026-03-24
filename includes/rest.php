429abec889d21182a678268fdd8acee8fab5499b
Jamshed <jamshedchhapra@gmail.com>
2026-03-24 19:35:09 +0500

    Updated rest.php file

--- includes/rest.php
+++ includes/rest.php
@@ -1,17 +1,29 @@
 <?php
+/* ===== START REST ===== */
 
-add_action('rest_api_init',function(){
+add_action('rest_api_init', function () {
 
-register_rest_route('jwem/v1','events',[
-'methods'=>'GET',
-'callback'=>function(){
+    register_rest_route('jwem/v1', '/events', [
+        'methods'  => 'GET',
 
-return get_posts([
-'post_type'=>'jwem_event',
-'numberposts'=>5
-]);
+        /* ===== START CALLBACK ===== */
+        'callback' => function () {
 
-}
-]);
+            return get_posts([
+                'post_type'   => 'jwem_event',
+                'numberposts' => 5
+            ]);
+
+        },
+        /* ===== END CALLBACK ===== */
+
+
+        /* ===== START PERMISSION CALLBACK ===== */
+        'permission_callback' => '__return_true'
+        /* ===== END PERMISSION CALLBACK ===== */
+
+    ]);
 
 });
+
+/* ===== END REST ===== */
